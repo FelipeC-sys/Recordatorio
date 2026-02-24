e7b690f912fba1f055cd871e65e9c11c9f811329
Andres Felipe Gallego Carvajal <9felipegc@gmail.com>
2026-02-23 23:00:52 -0500

    Se hicieron los atributos privados
    Added getter methods for nombre and hora.

--- RECORD/Actividad.java
+++ RECORD/Actividad.java
@@ -1,10 +1,18 @@
-class Actividad {
-    String nombre;
-    String hora;
+public class Actividad {
+
+    private String nombre;
+    private String hora;
 
     public Actividad(String nombre, String hora) {
         this.nombre = nombre;
         this.hora = hora;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getHora() {
+        return hora;
+    }
 }
